2e03fd89635ed6212de551f7b37e876fdfcf3238
sruly chaimowitz <srulychaimo@gmail.com>
2022-02-25 13:19:35 +0200

    רקדכםמדןהק

--- javascript/contect.js
+++ javascript/contect.js
@@ -1,24 +1,24 @@
-const nameInp = document.getElementById("name");
-const nameInpSmall = document.getElementById("nameSmall");
-const lastNameInp = document.getElementById("lastName");
-const lastNameInpSmall = document.getElementById("lastNameSmall");
-const emailInp = document.getElementById("email");
-const emailInpSmall = document.getElementById("emailSmall");
-const emailMassage = document.getElementById("spanEmail");
-let userEmailInp = [];
+// const nameInp = document.getElementById("name");
+// const nameInpSmall = document.getElementById("nameSmall");
+// const lastNameInp = document.getElementById("lastName");
+// const lastNameInpSmall = document.getElementById("lastNameSmall");
+// const emailInp = document.getElementById("email");
+// const emailInpSmall = document.getElementById("emailSmall");
+// const emailMassage = document.getElementById("spanEmail");
+// let userEmailInp = [];
 
-function pushIn() {
-  if (emailInp.value) {
-    userEmailInp.push(emailInp.value);
-  } else if (emailInpSmall.value) {
-    userEmailInp.push(emailInpSmall.value);
-  }
-}
-function testEmail() {
-  pushIn();
+// function pushIn() {
+//   if (emailInp.value) {
+//     userEmailInp.push(emailInp.value);
+//   } else if (emailInpSmall.value) {
+//     userEmailInp.push(emailInpSmall.value);
+//   }
+// }
+// function testEmail() {
+//   pushIn();
 
-  if (!userEmailInp[0].includes("@")) {
-    emailMassage.innerHTML +=
-      "<span class=' text-danger'> ** You have entered an invalid email address</span>";
-  }
-}
+//   if (!userEmailInp[0].includes("@")) {
+//     emailMassage.innerHTML +=
+//       "<span class=' text-danger'> ** You have entered an invalid email address</span>";
+//   }
+// }
---
+++ test/script.js
@@ -0,0 +1,12 @@
+// const emailInp = document.getElementById("email").value;
+
+// function emailValidation() {
+//   let userEmailInp = [];
+//   userEmailInp.push(emailInp.value);
+
+//   if (userEmailInp.includes("@")) {
+//   }
+// }
+
+// console.log(userEmailInp);
+// console.log(emailInp);
